c3c752d0531ccc6da425f70d7427496afd8acbd5
Vitali Barozzi <vitalivitor@gmail.com>
2018-12-03 02:23:44 -0200

    prettified

--- debian_debloat.sh
+++ debian_debloat.sh
@@ -1,56 +1,58 @@
 #!/usr/bin/env bash
 # To remove stuff i dont like from Debian
 
-sudo apt --yes remove gnome-calendar
-sudo apt --yes remove gnome-terminal
-sudo apt --yes remove gnome-weather
-sudo apt --yes remove gnome-clocks
-sudo apt --yes remove gnome-contacts
-sudo apt --yes remove gnome-tetravex
-sudo apt --yes remove gnome-chess
-sudo apt --yes remove gnome-sudoku
-sudo apt --yes remove gnome-klotski
-sudo apt --yes remove gnome-dictionary
-sudo apt --yes remove gnome-maps
-sudo apt --yes remove gnome-mines
-sudo apt --yes remove gnome-robots
-sudo apt --yes remove gnome-nibbles
-sudo apt --yes remove gnome-mahjongg
-sudo apt --yes remove gnome-taquin
+declare -r remove="sudo apt --yes remove"
 
-sudo apt --yes remove khmerconverter
-sudo apt --yes remove libreoffice-help-km
-sudo apt --yes remove task-khmer-desktop
-sudo apt --yes remove task-khmer
-sudo apt --yes remove fonts-khmeros
+$remove gnome-calendar
+$remove gnome-terminal
+$remove gnome-weather
+$remove gnome-clocks
+$remove gnome-contacts
+$remove gnome-tetravex
+$remove gnome-chess
+$remove gnome-sudoku
+$remove gnome-klotski
+$remove gnome-dictionary
+$remove gnome-maps
+$remove gnome-mines
+$remove gnome-robots
+$remove gnome-nibbles
+$remove gnome-mahjongg
+$remove gnome-taquin
 
-sudo apt --yes remove mozc-data
-sudo apt --yes remove mozc-server
-sudo apt --yes remove mozc-utils-gui
-sudo apt --yes remove uim-mozc
+$remove khmerconverter
+$remove libreoffice-help-km
+$remove task-khmer-desktop
+$remove task-khmer
+$remove fonts-khmeros
 
-sudo apt --yes remove synaptic
-sudo apt --yes remove scim
-sudo apt --yes remove mlterm
-sudo apt --yes remove tali
-sudo apt --yes remove evolution
-sudo apt --yes remove thunderbird
-sudo apt --yes remove brasero
-sudo apt --yes remove cheese
-sudo apt --yes remove goldendict
-sudo apt --yes remove four-in-a-row
-sudo apt --yes remove anthy
-sudo apt --yes remove kasumi
-sudo apt --yes remove libhdate1
-sudo apt --yes remove xiterm+thai
-sudo apt --yes remove iagno
-sudo apt --yes remove swell-foop
-sudo apt --yes remove five-or-more
-sudo apt --yes remove hitori
-sudo apt --yes remove shotwell
-sudo apt --yes remove lightsoff
-sudo apt --yes remove quadrapassel
-sudo apt --yes remove rhythmbox
-sudo apt --yes remove polari
+$remove mozc-data
+$remove mozc-server
+$remove mozc-utils-gui
+$remove uim-mozc
+
+$remove synaptic
+$remove scim
+$remove mlterm
+$remove tali
+$remove evolution
+$remove thunderbird
+$remove brasero
+$remove cheese
+$remove goldendict
+$remove four-in-a-row
+$remove anthy
+$remove kasumi
+$remove libhdate1
+$remove xiterm+thai
+$remove iagno
+$remove swell-foop
+$remove five-or-more
+$remove hitori
+$remove shotwell
+$remove lightsoff
+$remove quadrapassel
+$remove rhythmbox
+$remove polari
 
 sudo apt --yes autoremove

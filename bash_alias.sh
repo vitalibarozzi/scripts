fa493add5f4ec6d52afca54b526dd1165614ca54
Vitali Barozzi <vitalivitor@gmail.com>
2018-12-28 03:51:10 -0200

    fixed diff

--- bash_alias.sh
+++ bash_alias.sh
@@ -1,24 +1,27 @@
 #!/usr/bin/env bash
 # Hold aliases for common used commands
 
-# Quick Ones =================================================================
-alias c      = "clear";
-alias e      = "exit \$?";
-alias s      = "sudo";
-alias m      = "make";
-# General ====================================================================
-alias     ls = 'ls --color      = always --group-directories-first --almost-all --human-readable --sort = extension -l';
-alias umount = 'sudo umount';
-alias   grep = 'grep --color';
-# Git ========================================================================
-alias   diff = 'git diff';
-alias  clone = 'git clone';
-alias branch = 'git branch';
-alias    add = 'git add';
-alias status = 'git status';
-alias commit = 'git commit -m';
-alias    log = 'git log';
-alias   push = 'git push';
-# Haskell ====================================================================
+# Quick Ones 
+alias      c="clear";
+alias      e="exit \$?";
+alias      s="sudo";
+alias      m="make";
+
+# General 
+alias     ls='ls --group-directories-first --almost-all --human-readable --sort=extension -l';
+alias umount='sudo umount';
+alias   grep='grep --color';
+
+# Git 
+alias   diff='git difftool';
+alias  clone='git clone';
+alias branch='git branch';
+alias    add='git add';
+alias status='git status';
+alias commit='git commit -m';
+alias    log='git log';
+alias   push='git push';
+
+# Haskell 
 alias ghci-core="ghci -ddump-simpl -dsuppress-idinfo -dsuppress-coercions -dsuppress-type-applications -dsuppress-uniques -dsuppress-module-prefixes"
 

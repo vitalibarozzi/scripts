87558e81115d571fde60b1383ad98a12844d9ab6
Vitali Barozzi <vitalivitor@gmail.com>
2019-01-04 23:29:21 -0200

    renamed backup file

--- bash_jumpstart.sh
+++ bash_jumpstart.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-. "./bash_conf.sh"   || exit 2;
-. "./bash_func.sh"   || exit 3;
-. "./bash_path.sh"   || exit 4;
-. "./bash_alias.sh"  || exit 5;
-. "./bash_macros.sh" || exit 6;
+. "./bash_conf.sh"   || exit 1;
+. "./bash_func.sh"   || exit 2;
+. "./bash_path.sh"   || exit 3;
+. "./bash_alias.sh"  || exit 4;
+. "./bash_backup.sh" || exit 5;

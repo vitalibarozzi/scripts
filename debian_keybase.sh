d14bbcc80017ea77a151f3076f10972cedc66411
Vitali Barozzi <vitalivitor@gmail.com>
2019-03-04 08:42:32 -0300

    keybase file

---
+++ debian_keybase.sh
@@ -0,0 +1,10 @@
+# To install keybase.
+# From: https://keybase.io/docs/the_app/install_linux
+
+curl --remote-name https://prerelease.keybase.io/keybase_amd64.deb
+# if you see an error about missing `libappindicator1`
+# from the next command, you can ignore it, as the
+# subsequent command corrects it
+sudo dpkg -i keybase_amd64.deb
+sudo apt-get install -f
+run_keybase
